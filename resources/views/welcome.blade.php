cab61d74c199a46dcd8bcd4f1d9ecc73c10b3e88
FayyadhMifay <1941720132@student.polinema.ac.id>
2021-02-19 09:24:04 +0700

    kode program ketiga

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-            <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Fayyadh Al Baity</h1></div>
+            <div class="ml-4 text-lg leading-7 font-semibold"><h1 class="text-gray-900 dark:text-white">Fayyadh Al Baity 1941720132</h1></div>
         </div>
     </body>
 </html>
